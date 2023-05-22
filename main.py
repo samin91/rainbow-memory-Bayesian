09444b618924f7b7babf71b05fdcaa9fe213f8c9
samin91 <saminhamidi@yahoo.com>
2023-05-22 07:48:31 +0200

    Add ray tun to the main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ rainbow-memory
 Copyright 2021-present NAVER Corp.
 GPLv3
 """
+from functools import partial
+
 import datetime
 import logging.config
 import os
@@ -18,21 +20,37 @@ from torch import nn
 from torch.utils.tensorboard import SummaryWriter
 from torchvision import transforms
 
+# Ray
+import ray
+from ray import tune, air
+from ray.tune import CLIReporter
+from ray.tune.schedulers import ASHAScheduler
+from ray.tune.search.hyperopt import HyperOptSearch
+from ray.tune.search.bayesopt import BayesOptSearch
+
 from configuration import config
 from utils.augment import Cutout, select_autoaugment
+
+# ray.tune 
 from utils.data_loader import get_test_datalist, get_statistics
 from utils.data_loader import get_train_datalist, get_valid_datalist
+
 from utils.method_manager import select_method
 from utils.bayes_utils import configure_prior_conversion_function
 from losses.probabilistic_loss import configure_model_and_loss
 
+from methods.rm_trainable import trainable_RM
+
+
+
 # add the bayesian losses
 from losses import ClassificationLoss, ClassificationLossVI
 import pdb
+import pickle
 
 def main():
     
-    Exp_name = 'B_32'
+    Exp_name = 'test_ray'
     args = config.base_parser()
     # time stamp 
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
@@ -124,7 +142,7 @@ def main():
     logger.info(f"[1] Select a CIL method ({args.mode})")
 
 
-    # Load the checkpoint or pretrained model
+    # Load the checkpoint or pretrained model - pretraining the bayesian model for cub200
     if args.pretrain is True and args.bayesian_model is True:
         method.checkpoint_saver_loader()
 
@@ -156,8 +174,6 @@ def main():
         eval_dict = dict()
 
         # get datalist
-        # what about validation set?
-        
         cur_train_datalist = get_train_datalist(args, cur_iter)
         cur_valid_datalist = get_valid_datalist(args, args.exp_name, cur_iter)
         cur_test_datalist = get_test_datalist(args, args.exp_name, cur_iter)
@@ -174,24 +190,126 @@ def main():
         # Increment known class for current task iteration.
         # reinitalize optimizer and scheduler?
         # Add the trainable parameters for the optimizer
-        method.before_task(cur_train_datalist, cur_iter, args.init_model, args.init_opt, args.bayesian_model)
 
-        # The way to handle streamed samles
+        method.before_task(cur_train_datalist, cur_iter, args.init_model, args.init_opt, 
+                           args.bayesian_model)
+
+        # The way to handle streamed samples
         logger.info(f"[2-3] Start to train under {args.stream_env}")
-
+        
         if args.stream_env == "offline" or args.mode == "joint" or args.mode == "gdumb":
             # Offline Train
             # returns best eval accuracy and not the last one
             # task_acc is the evaluation accuracy and not the training accuracy
-            task_acc, eval_dict = method.train(
-                cur_iter=cur_iter,
-                n_epoch=args.n_epoch,
-                batch_size=args.batchsize,
-                n_worker=args.n_worker,
-                writer=writer
-            )
-            if args.mode == "joint":
-                logger.info(f"joint accuracy: {task_acc}")
+        
+            # -----------------------------------------------------------------------------------------------------------------
+            # Ray Tune for the first task of the blurry case
+            # -----------------------------------------------------------------------------------------------------------------
+            
+            if args.exp_name == "blurry10" and cur_iter==0:
+                # configs has already been defined.
+                
+                configs={"lr": tune.loguniform(1e-4, 1e-1), 
+                    "weight_decay": tune.uniform(1e-8, 1e-1)
+                    }
+                hyperopt_search = HyperOptSearch(metric='accuracy', mode='max')
+                #hyperopt_search = BayesOptSearch(metric='loss', mode='min',points_to_evaluate=[{"lamda": 1}, {"lamda": 25}]
+                scheduler = ASHAScheduler(
+                    metric="accuracy",
+                    mode="max",
+                    max_t=100,
+                    grace_period=5,
+                    reduction_factor=2)
+                
+                reporter = CLIReporter(
+                    parameter_columns=["lr", "wd"],
+                    metric_columns=["loss", "accuracy", "training_iteration"]
+                    )
+                
+                #pickle.dumps(tune.with_parameters(method.find_hyperparametrs))
+                
+                '''
+                def train_fn(config):
+                    method.train(
+                        cur_iter=cur_iter,
+                        n_epoch=args.n_epoch,
+                        batch_size=args.batchsize,
+                        n_worker=args.n_worker,
+                        writer=writer,
+                        **config)
+                '''
+
+                result = tune.run(
+                                method.find_hyperparametrs,
+                                #max_concurrent_trials=2,  # adding this to avoid the paralelism issue? 
+                                resources_per_trial={"cpu": 24, "gpu": 0.5},
+                                config=configs,
+                                num_samples=1,
+                                search_alg=hyperopt_search,
+                                scheduler=scheduler,
+                                #keep_checkpoints_num=2,
+                                checkpoint_score_attr="accuracy", 
+                                #progress_reporter=reporter
+                                )
+               
+                '''
+                # this is another way of doing it! 
+                tuner = tune.Tuner(
+                    method,
+                    run_config = air.RunConfig(stop={"accuracy": 0.95,"training_iteration":1,},checkpoint_config=air.CheckpointConfig(checkpoint_at_end=True, checkpoint_frequency=3),),
+                    tune_config=tune.TuneConfig(search_alg=hyperopt_search, scheduler=scheduler, num_samples=1,),
+                    param_space={"lr": tune.loguniform(1e-4, 1e-1), "weight_decay":tune.uniform(1e-8, 1e-1)},
+                    )
+                    #tuner = tune.Tuner.restore(path="/visinf/home/shamidi/ray_results/RM_trainable_2023-05-19_14-48-56")
+                result_grid = tuner.fit()   
+            
+                num_results = len(result_grid)
+
+                # Check if there have been errors
+                if result_grid.errors:
+                    print("At least one trial failed.")
+
+                # Get the best result
+                best_result = result_grid.get_best_result()
+
+                # And the best checkpoint
+                best_checkpoint = best_result.checkpoint
+
+                # And the best metrics
+                best_metric = best_result.metrics
+                '''            
+        
+                best_trial = result.get_best_trial("accuracy", "max", "last")
+                logging.info("Best trial config: {}".format(best_trial.config))
+                logging.info("Best trial final validation loss: {}".format(best_trial.last_result["loss"]))
+                logging.info("Best trial final validation accuracy: {}".format(best_trial.last_result["accuracy"]))
+
+                best_checkpoint = result.get_best_checkpoint(trial=best_trial, metric="accuracy", mode="max")
+                best_checkpoint_dir = best_checkpoint.to_directory(path="directory")
+                model_state, optimizer_state = torch.load(os.path.join(best_checkpoint_dir, "checkpoint"))
+
+                #best_trained_model = model._network
+                #best_trained_model.load_state_dict(model_state)
+                method.self.model.load_state_dict(model_state)
+                #model.check_fisher()
+                method.self.model.load_state_dict(model_state)
+
+                TEST_LOADER = method.get_dataloader(batch_size=args.batch_size, n_worker=args.n_worker, train_list=None, test_list=method.self.test_list, valid_list=None)
+                test_dictionary=method.evaluation(TEST_LOADER, method.self.criterion)
+                print("Best trial test set accuracy: {}".format(test_dictionary["avg_acc"]))
+            
+            # ---------------------------------------------------------------------------------------------------------------------------------------------------------------
+            else: 
+            
+                task_acc, eval_dict = method.find_hyperparametrs(
+                    cur_iter=cur_iter,
+                    n_epoch=args.n_epoch,
+                    batch_size=args.batchsize,
+                    n_worker=args.n_worker,
+                    writer=writer
+                )
+                if args.mode == "joint":
+                    logger.info(f"joint accuracy: {task_acc}")
 
         elif args.stream_env == "online":
             # Online Train
